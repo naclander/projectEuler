c384c13f592d5dca11ba60b6d9c2077f70ec0432
Nathan Aclander <naclander@linux.ucla.edu>
2013-12-13 14:31:28 -0800

    Completed problem 7 in C

---
+++ problem7/problem7.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+//Uses a simple (inneficient) method of finding primes.
+
+#define TARGET 10001
+
+int is_prime(int num){
+  int i;
+  for(i = 2; i <= num/2; i++){
+    if(num%i == 0){
+	  return 0;
+	}
+  }
+  return 1;
+}
+
+int main(){
+  int count_primes = 0;
+  int i;
+  for(i = 2;;i++){
+    if(is_prime(i)){
+	  count_primes++;
+	}
+	if(count_primes == TARGET){
+	  printf("%d\n",i);
+	  return;
+	}
+  }
+}
